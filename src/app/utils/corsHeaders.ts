b089fd0ee56a616dd60635b58b08c22144b359fb
Bogdan <bogdan.pavliv@gmail.com>
2025-03-04 16:26:46 +0200

    add project

---
+++ src/app/utils/corsHeaders.ts
@@ -0,0 +1,10 @@
+export const corsHeaders = {
+  headers: {
+    'Access-Control-Allow-Origin': 'https://pixelmagnet-admin-mern-server.netlify.app',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  },
+}
+
+
+// 'Access-Control-Allow-Origin': 'https://rostelecom-admin.vercel.app',
