ed4927e43743c50cee10c6138b6e5125ea9d9edc
Bogdan <bogdan.pavliv@gmail.com>
2025-03-04 16:15:37 +0200

    add project

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,18 @@
 const nextConfig = {
     images: {
       remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: 'localhost',
-          port: '3002',
-          pathname: '/**',
-        },
+        { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+        { protocol: 'http', hostname: 'localhost' },
+        { protocol: 'https', hostname: 'pixelmagnet-mongodb-client.netlify.app' },
       ],
     },
   };
   
   export default nextConfig;
   
+  // {
+  //   protocol: 'http',
+  //   hostname: 'localhost',
+  //   port: '3002',
+  //   pathname: '/**',
+  // },
--- src/app/utils/axios.ts
+++ src/app/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'pixelmagnet-admin-mern-server.netlify.app/api',
+    baseURL: 'https://pixelmagnet-admin-mern-server.netlify.app/api',
     validateStatus: () => true,
 })
 
