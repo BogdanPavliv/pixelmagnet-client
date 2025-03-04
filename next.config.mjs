c4421a254ab60f83260e3da0302567ded867261e
Bogdan <bogdan.pavliv@gmail.com>
2025-03-04 19:31:50 +0200

    add project

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-        { protocol: 'http', hostname: 'localhost' },
-        { protocol: 'https', hostname: 'pixelmagnet-mongodb-client.netlify.app' },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
-  // {
-  //   protocol: 'http',
-  //   hostname: 'localhost',
-  //   port: '3002',
-  //   pathname: '/**',
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3002',
+        pathname: '/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://pixelmagnet-admin-mern-server.netlify.app/api/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
